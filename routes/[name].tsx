feb8057eef3e624ac32f6b9e7766314b148ab678
Mario Martinez <mmartinez@energy-tools.net>
2022-12-21 23:58:55 +0100

    fix show message response

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -15,5 +15,5 @@ export const handler: Handlers<any> = {
 };
 
 export default function Greet(props: PageProps<any>) {
-  return <div><img src={props.data.data[0].url} alt="generated image" /> {props.data[0].url}</div>;
+  return <div><img src={props.data.data[0].url} alt="generated image" /> {props.data.data[0].url}</div>;
 }
